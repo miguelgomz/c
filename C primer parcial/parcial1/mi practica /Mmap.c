66b36767544bd2880922f77da56213bfe57d382a
miguelgomz <miguelangelgomezmiranda06@gmail.com>
2026-04-11 11:01:06 +0200

    mmap

---
+++ C primer parcial/parcial1/mi practica /Mmap.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+
+
+int main(){
+
+    int *pizarra= mmap(NULL,sizeof(int), PROT_READ | PROT_WRITE , MAP_SHARED| MAP_ANONYMOUS ,-1, 0);
+    
+    pid_t hijo=fork();
+
+    int numero_por_consola;
+
+    if (hijo>0){
+
+    printf("Escribe un numero por consola de favor ");
+    scanf("%d",&numero_por_consola);
+
+     *pizarra=numero_por_consola;
+
+    wait(NULL);
+    
+    printf("El numero lo veo y vale %d",*pizarra);
+
+    }else if(hijo==0){
+
+        sleep(10);
+        printf("Yo soy el hijo y el valor es %d",*pizarra);
+        
+        *pizarra= *pizarra + 1;
+
+        printf("Soy El hijo y modifique el valor ");
+    }
+    munmap(pizarra,sizeof(int));
+    return 0;
+
+}
