b73e50d2189f4e45dc27ba852a6d9a854a694b64
miguelgomz <miguelangelgomezmiranda06@gmail.com>
2026-05-04 18:55:22 +0200

    Hilos sencillos para aprender

---
+++ ReentederHilos.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+
+void*hola(void*arg){
+    printf("hola quiero imprimir este mensaje");
+    return NULL;
+}
+int main (){
+pthread_t hilo1;
+
+pthread_create(&hilo1,NULL,hola,NULL);
+pthread_join(hilo1,NULL);
+}
+
+//Hola re captando lo que era hacer hilos 
